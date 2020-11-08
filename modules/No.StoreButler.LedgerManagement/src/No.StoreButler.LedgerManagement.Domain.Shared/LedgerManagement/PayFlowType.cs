6c7d6391e3210dfcd946c9e44491390cd4ee4ada
tfcye <whoisye@outlook.com>
2020-11-08 22:36:07 +0800

    Add PayFLowType

--- modules/No.StoreButler.LedgerManagement/src/No.StoreButler.LedgerManagement.Domain.Shared/LedgerManagement/PayFlowType.cs
+++ modules/No.StoreButler.LedgerManagement/src/No.StoreButler.LedgerManagement.Domain.Shared/LedgerManagement/PayFlowType.cs
@@ -1,7 +1,14 @@
 ﻿namespace No.StoreButler.LedgerManagement.LedgerManagement
 {
+    /// <summary>
+    /// 资金流向类型
+    /// </summary>
     public enum PayFlowType
     {
-        
+        // 收入
+        Income,
+
+        // 支出
+        Expenses
     }
 }
